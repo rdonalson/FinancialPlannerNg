46bceb25ec40231c48a3dfcdc347e2aa8ef5fa4a
Rick Donalson <rick.donalson@me.com>
2021-06-17 08:38:17 -0400

    Moved Test items to appropriate Test folders

--- FPNg-API/FPNg.API.Data/Context/FPNGContext.cs
+++ FPNg-API/FPNg.API.Data/Context/FPNGContext.cs
@@ -7,7 +7,7 @@ namespace FPNg.API.Data.Context
 {
     public partial class FPNgContext : DbContext
     {
-        // private readonly string Conn = "Data Source=DESKTOP-VPBJU0V\\APPLICATION;Initial Catalog=FinancialPlannerNg;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;";
+        private readonly string Conn = "";  // When creating structure add a connection string
 
         public FPNgContext() { }
         public FPNgContext(DbContextOptions<FPNgContext> options) : base(options) { }
